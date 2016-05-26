19ccedac5f50cb12f7c07593dddf6feb6473c5e0
AprilOilecDeaniellePacayra <april_pacayra@dlsu.edu.ph>
2016-05-26 16:41:37 +0800

    Create Personal Information.c

---
+++ Personal Information.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+	int a;
+	
+	printf("\t\t\t");
+	for (a=1;a<28;a++)
+	printf("@");
+	printf("\n\n\t\t     April Oilec Deanielle B. Pacayra\n");
+	printf("\n\t\t\t\t   BS CIV\n");
+	printf("\n\t\t\t\t    114\n");
+	printf("\n\t\t\t       May 26, 2016\n\n");
+	
+	printf("\t\t\t");
+	for (a=1;a<28;a++)
+	printf("@");
+	printf("\n\n");
+	
+	system("PAUSE");
+	return 0;
+}
